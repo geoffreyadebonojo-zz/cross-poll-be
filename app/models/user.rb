6fb43c0dd8013c5753d0685b48fd2b0cc545feba
Meghan Stang <mstang15@gmail.com>
2019-02-12 13:23:40 -0700

    Add email to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :username,
-                        :first_name,
-                        :last_name
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email,
                         :password
 
   validates :email, presence: true, uniqueness: true
