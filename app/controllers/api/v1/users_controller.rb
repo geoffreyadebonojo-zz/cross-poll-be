82fb66bf6974b2ffd6fbe745f17527b93fab4647
Meghan Stang <mstang15@gmail.com>
2019-02-12 12:56:32 -0700

    Remove username column from user table since we are using email as username

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::UsersController<ApplicationController
 
   def create
     user = User.create(user_params) if matching_passwords?
+    if user.save
+      # session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_022115) do
+ActiveRecord::Schema.define(version: 2019_02_12_195543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
