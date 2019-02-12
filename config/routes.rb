3cce905ad2c767efb30eb9461e1b76684b62fbd0
Meghan Stang <mstang15@gmail.com>
2019-02-12 13:24:23 -0700

    Change register to users in our routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :register, only: :create do
+      resources :users, only: :create do
       end
     end
   end
