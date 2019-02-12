c7558fda000d0cf249a231a9a02f598b13b8fb72
Meghan Stang <mstang15@gmail.com>
2019-02-12 13:45:49 -0700

    Initialize UserSerialzer with user object

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,9 +3,7 @@ class Api::V1::UsersController<ApplicationController
   def create
     user = User.create(user_params) if matching_passwords?
     if user.save
-      # session[:user_id] = user.id
-    else
-      render json: UserSerializer.new({message: successfully_created_message(user), user_id: user.id})
+      render json: UserSerializer.new(user)
     end
   end
 
@@ -17,10 +15,6 @@ class Api::V1::UsersController<ApplicationController
 
   def matching_passwords?
     params[:password] == params[:password_confirmation]
-  end
-
-  def successfully_created_message(user)
-    "Account successfully created for #{user.first_name} #{user.last_name}."
   end
 
 end
