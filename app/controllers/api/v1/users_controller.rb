cd4f69d9723a0bc639f142a4b21feadbaedd270c
Meghan Stang <mstang15@gmail.com>
2019-02-13 13:40:44 -0700

    Add error message for when user email is already used

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,23 @@
 class Api::V1::UsersController<ApplicationController
 
   def create
-    user = User.create(user_params) if matching_passwords?
-    if user!= nil && user.save
-      render json: UserSerializer.new(user)
+    if current_user
+      render :json => {:error => "There is already an account created with this email address. Check your password, or register with a different email address to log in."}.to_json, :status => 401
     else
-      render :json => {:error => "Make sure passwords match, and a proper email address was provided"}.to_json, :status => 400
+      user = User.create(user_params) if matching_passwords?
+      if user!= nil && user.save
+        render json: UserSerializer.new(user)
+      else
+        render :json => {:error => "Make sure passwords match, and a proper email address was provided"}.to_json, :status => 400
+      end
     end
   end
 
   private
+
+  def current_user
+    User.where(email: user_params[:email]) != []
+  end
 
   def user_params
     params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :api_token)
