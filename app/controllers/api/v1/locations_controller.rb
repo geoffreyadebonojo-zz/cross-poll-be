0d1087988036e0ae30714394e9a9baa8f0c0d7ab
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-14 15:21:36 -0700

    test travis

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::LocationsController < ApplicationController
   def index
+    
     location = request.location.ip
     zip_code = LocationService.new(location).get_zip[9].first[1]
     render :json => {:zip_code => zip_code}.to_json, :status => 200
