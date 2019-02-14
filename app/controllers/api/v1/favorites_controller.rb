0d6fe89e987f23f798205f31a14db73d1c028a19
Meghan Stang <mstang15@gmail.com>
2019-02-14 14:40:54 -0700

    Add json messages for successfull and errored post requests for saving favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::FavoritesController < ApplicationController
 
   def create
     user = User.find_by(api_token: params[:api_token])
-    user.favorites.create(favorite_id: params[:favorite_id])
-    binding.pry
+    if user != nil
+      user.favorites.create(favorite_id: params[:favorite_id])
+      render :json => {:message => "Successfully added favorite with id #{params[:favorite_id]}"}.to_json, :status => 200
+    else
+      render :json =>{:error => "Cannot find user with this api token"}.to_json, :status => 404
+    end
   end
 
 end
