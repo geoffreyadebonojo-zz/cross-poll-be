4f10131e7b53f5c7f6bc7d0238c428638fececf1
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-14 14:37:03 -0700

    Create geolocation test

---
+++ spec/requests/api/v1/user_can_get_geolocation_from_api_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Geolocation Endpoint" do
+  it "GET /location" do
+    get "/api/v1/locations"
+    expect(response.status).to eq(200)
+    expect(response.keys[0]).to eq("zip_code")
+    expect(response["keys"]).to_be a(String)
+  end
+end
