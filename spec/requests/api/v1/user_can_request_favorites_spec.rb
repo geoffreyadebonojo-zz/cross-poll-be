fec25e27799484509c0ed542acd173c74ea4e9cc
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-02-14 19:48:09 -0700

    Add delete favorite test and error path

--- spec/requests/api/v1/user_can_request_favorites_spec.rb
+++ spec/requests/api/v1/user_can_request_favorites_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Favorites Endpoint" do
     expect(body.count).to eq(2)
   end
 
-  it "throws an error if incorrect api token is given" do
+  it "GET throws an error if incorrect api token is given" do
     user_data = {
       "first_name": "The Greatest",
       "last_name": "Guy",
@@ -66,7 +66,7 @@ RSpec.describe "Favorites Endpoint" do
     expect(JSON.parse(response.body)["message"]).to eq("Successfully added favorite with id 1")
   end
 
-  it "throws an error if no api token is provided" do
+  it "POST throws an error if no api token is provided" do
     user_data = {
       "first_name": "The Greatest",
       "last_name": "Guy",
@@ -85,7 +85,46 @@ RSpec.describe "Favorites Endpoint" do
     expect(response.status).to eq(404)
   end
 
-  xit "DELETE /favorites" do
+  it "DELETE /favorites" do
+    user_data = {
+      "first_name": "The Greatest",
+      "last_name": "Guy",
+      "email": "ggg@gmail.com",
+      "password": "abc123doremi",
+      "password_confirmation": "abc123doremi"
+    }
 
+    user = User.create!(user_data)
+    user.favorites.create!(favorite_id: 1)
+    user.favorites.create!(favorite_id: 2)
+
+    data = {
+      "api_token": user.api_token,
+      "favorite_id": 1
+    }
+
+    delete "/api/v1/favorites/1", params: data
+    expect(response.status).to eq(200)
+    body = JSON.parse(response.body)["data"]
+    expect(body.count).to eq(1)
+  end
+
+  it "DELETE throws an error if no api token is provided" do
+    user_data = {
+      "first_name": "The Greatest",
+      "last_name": "Guy",
+      "email": "ggg@gmail.com",
+      "password": "abc123doremi",
+      "password_confirmation": "abc123doremi"
+    }
+
+    user = User.create!(user_data)
+
+    data = {
+      "favorite_id": 1
+    }
+
+    delete "/api/v1/favorites", params: data
+    expect(response.status).to eq(404)
   end
 end
