3037c2ebd0e64ee3e1e0cca1cf73210ec050cfc8
Meghan Stang <mstang15@gmail.com>
2019-02-16 20:33:24 -0700

    Add route for getting distance between current location and shelter location

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: :create 
-      resources :sessions 
-      resources :locations 
-      resources :favorites 
+      resources :users, only: :create
+      resources :sessions
+      resources :locations
+      resources :favorites
       delete '/favorites', to: 'favorites#destroy'
       post '/shelter_notifier', to: 'shelter_notifier#create'
+      get '/distances', to: 'distances#index'
     end
   end
 
