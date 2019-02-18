d2610507f7da5e6a15d7045dbe4bab29b8d96f8d
Meghan Stang <mstang15@gmail.com>
2019-02-18 13:41:30 -0700

    Add create method for sending text messages

---
+++ app/controllers/api/v1/texts_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::TextsController < ApplicationController
+
+  def create
+    user = User.find_by(api_token: params[:api_token])
+    twilio = TwilioFacade.new(user, params[:recipient_phone], params[:pic], params[:pet_name])
+    twilio.notify_friend
+  end
+
+end
