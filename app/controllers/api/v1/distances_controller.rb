51d90a59a4f4c7f110f817aebd34f775cbc45507
Meghan Stang <mstang15@gmail.com>
2019-02-19 14:09:32 -0700

    Add tests for distance controller and model

--- app/controllers/api/v1/distances_controller.rb
+++ app/controllers/api/v1/distances_controller.rb
@@ -2,11 +2,18 @@ class Api::V1::DistancesController < ApplicationController
 
   def index
     mileage = DistanceFacade.new(params[:user_lat], params[:user_long], params[:shelter_lat], params[:shelter_long]).get_distance
-    if mileage.class == Integer
+    if mileage.class == Integer && all_params_present
       render :json => {:distance => mileage}.to_json, :status => 200
     else
       render :json => {:error => "Mileage could not be calculated"}.to_json, :status => 400
     end
+  end
+
+
+  private
+
+  def all_params_present
+  params.keys.count == 6 && params[:user_lat] != nil && params[:user_long]!=nil && params[:shelter_lat]!=nil && params[:shelter_long]!= nil
   end
 
 end
