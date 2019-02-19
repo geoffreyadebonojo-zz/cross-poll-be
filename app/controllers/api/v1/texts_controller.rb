d227867170c8e21b9ea644b0a843fd92c1d0b6f6
Meghan Stang <mstang15@gmail.com>
2019-02-19 11:25:26 -0700

    Add success message for sent text, error message is still wip

--- app/controllers/api/v1/texts_controller.rb
+++ app/controllers/api/v1/texts_controller.rb
@@ -3,7 +3,11 @@ class Api::V1::TextsController < ApplicationController
   def create
     user = User.find_by(api_token: params[:api_token])
     twilio = TwilioFacade.new(user, params[:recipient_phone], params[:pic], params[:pet_name], params[:shelter_name])
-    twilio.notify_friend
+    if twilio.notify_friend
+      render :json => {:success => "Text Sent!"}.to_json, :status => 200
+    else
+      render :json => {:error => "This number is not currently verified with our Twilio Trial Account"}.to_json, :status => 500   #this doesnt work yet
+    end
   end
 
 end
