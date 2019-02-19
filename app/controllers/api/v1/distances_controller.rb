38970a8b041eec1b9cfde01cc924115ab7c6bbc2
Meghan Stang <mstang15@gmail.com>
2019-02-19 11:09:37 -0700

    Add error message for when distance was not calculated correctly

--- app/controllers/api/v1/distances_controller.rb
+++ app/controllers/api/v1/distances_controller.rb
@@ -2,7 +2,10 @@ class Api::V1::DistancesController < ApplicationController
 
   def index
     mileage = DistanceFacade.new(params[:user_lat], params[:user_long], params[:shelter_lat], params[:shelter_long]).get_distance
-    render :json => {:distance => mileage}.to_json, :status => 200
+    if mileage.class == Integer
+      render :json => {:distance => mileage}.to_json, :status => 200
+    else
+      render :json => {:error => "Mileage could not be calculated"}.to_json, :status => 400
   end
 
 end
